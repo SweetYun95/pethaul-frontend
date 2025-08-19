aa86ba9ad3fa9dca4a4204f41ba95671050c1da2
SweetYun95 <isr01154@naver.com>
2025-08-19 20:35:57 +0900

    [fix] merge conflict leftover cleanup in OrderState

--- src/components/myInfo/OrderState.jsx
+++ src/components/myInfo/OrderState.jsx
@@ -151,22 +151,6 @@ function OrderState({ order }) {
             )}
          </div>
       </section>
-   ) : (
-      <section id="order-state">
-         <div className="contents-card top">
-            <div className="card-header">
-               <div className="window-btn">
-                  <span className="red"></span>
-                  <span className="green"></span>
-                  <span className="blue"></span>
-               </div>
-               <span className="card-title">주문현황</span>
-            </div>
-            <div>
-               <p>주문 내역이 존재하지 않습니다.</p>
-            </div>
-         </div>
-      </section>
    )
 }
 
