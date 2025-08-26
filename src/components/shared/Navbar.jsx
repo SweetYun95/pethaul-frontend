f3fda5e765e31462254d4b5a96b38f767534ddbc
eeinoeyes <loveourjune@gmail.com>
2025-08-26 14:34:05 +0900

    [patch] 일부 파일 수정(기능 변화 없음)

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -248,7 +248,7 @@ function Navbar() {
                      </NavLink>
                   </li>
                   <li>
-                     <NavLink to="/support">고객센터</NavLink>
+                     <NavLink to="/myQnAlist">고객센터</NavLink>
                   </li>
                </ul>
 
