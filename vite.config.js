a178646d3fc2fe0ee306bb3faa88fd09058c7746
Ubuntu <ubuntu@ip-172-31-5-102.ap-northeast-2.compute.internal>
2025-09-01 06:55:50 +0000

    [fix]배포중 수정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import eslintPlugin from 'vite-plugin-eslint' // ESLint 플러그인 추가
 
 // https://vite.dev/config/
 export default defineConfig({
    plugins: [
       react(),
-      eslintPlugin(), // ESLint 플러그인 추가
    ],
+   build: { sourcemap: true }
 })
